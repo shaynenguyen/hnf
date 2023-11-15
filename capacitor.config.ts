f1ec5877e6f43371532192e771fa3156351c89a4
ShayneNg <phamhai1986@yahoo.com>
2023-11-15 17:32:20 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hnf_sdk',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
